7fd9a09258e6c4bfc42412d8fd7f91d704190523
Samuel Aleixo <samuelmatriz@hotmail.com>
2022-06-07 15:25:28 -0300

    Ajuste paginas Html visualização.

---
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoMVC.Models.ViewModels;
+
+namespace ProjetoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Escreva sobre a empresa.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "O contato da empresa.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registre()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoMVC.Models;
+
+namespace ProjetoMVC.Data
+{
+    public class SalesWebMVCContext : DbContext
+    {
+        public SalesWebMVCContext (DbContextOptions<SalesWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoMVC.Models.Departament> Departament { get; set; }
+        public DbSet<ProjetoMVC.Models.Seller> Seller { get; set; }
+        public DbSet<ProjetoMVC.Models.SalesRecord> SalesRecord { get; set; }
+    }
+}
