65d2a5d7f30b51506b4f687c5baba9e6d95b32a3
Samuel Aleixo <samuelmatriz@hotmail.com>
2022-06-07 15:25:28 -0300

    Adicionar arquivos de projeto.

---
+++ SalesWebMVC/Services/DepartmentService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoMVC.Models;
+using ProjetoMVC.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoMVC.Services
+{
+    public class DepartmentService
+    {
+
+        private readonly SalesWebMVCContext _context;
+        public DepartmentService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Departament>> FindAllAsync()
+        {
+            return await _context.Departament.OrderBy(x=> x.Name).ToListAsync();
+        }
+
+    }
+}
